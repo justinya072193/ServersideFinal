e104ed868052eff433884865367d85bbd3178786
justinya072193 <justin38@uw.edu>
2019-05-12 18:34:52 -0700

    add

--- haiticoffee/haitiApp/urls.py
+++ haiticoffee/haitiApp/urls.py
@@ -3,13 +3,14 @@ from django.urls import path, include
 from . import views
 
 #this creates path 
-app_name = 'haitiApp'
-urlpatterns = [
-    path('carts', views.carts, name = "cart"),
-    path('carts', views.cartByID, name = "cartByID"),
-    path('orders', views.orders, name = "orders"),
-    path('orders', views.ordersByID, name = "ordersByIDS")
-    
+# app_name = 'haitiApp'
+# urlpatterns = [
+#     path('carts', views.carts, name = "cart"),
+#     path('carts', views.cartByID, name = "cartByID"),
+#     path('orders', views.orders, name = "orders"),
+#     path('orders', views.ordersByID, name = "ordersByIDS")
+# ]
+
 app_name = 'main'
 urlpatterns = [
     path('/users/address', views.address, name='address'),
