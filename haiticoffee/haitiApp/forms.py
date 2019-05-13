a90634f9a0d13b8e76edd1345b8ee9b9f47d7df9
Doug <doug.doenges@gmail.com>
2019-05-12 18:42:50 -0700

    finish group session 1

--- haiticoffee/haitiApp/forms.py
+++ haiticoffee/haitiApp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class NewAddressForm(forms.Form):
     addressLine1 = forms.CharField(label='Address Line 1', max_length=250, required=True)
-    addressLine2 = forms.CharField(label='Address Line 2', max_length=250, requied=False)
+    addressLine2 = forms.CharField(label='Address Line 2', max_length=250)
     city = forms.CharField(label='City', max_length=250, required=True)
     state = forms.CharField(label='State', max_length=30, required=True)
     postalCode = forms.CharField(label='Postal Code', max_length=10, required=True)
--- haiticoffee/haitiApp/urls.py
+++ haiticoffee/haitiApp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('carts', views.carts, name = "cart"),
     path('carts', views.cartByID, name = "cartByID"),
     path('orders', views.orders, name = "orders"),
-    path('orders', views.ordersByID, name = "ordersByIDS")
+    path('orders', views.ordersByID, name = "ordersByIDS"),
     path('/users/address', views.address, name='address'),
     path('/users/address/<int:address_id>', views.addressByID, name='addressByID')
 ]
